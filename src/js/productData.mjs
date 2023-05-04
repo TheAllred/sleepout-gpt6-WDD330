c50f51f766dc16abe32c13dfc716c15c4cf13c5e
jordankraude <jordankraudeTP@gmail.com>
2023-05-04 16:16:00 -0600

    wip

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -28,13 +28,44 @@ productDetails(productId, selector){
 
 productDetailsTemplate(product){
   return  `
-  <h3 id="productName"></h3>
+  <h3 id="productName">${product.Name}</h3>
   <h2 class="divider" id="productNameWithoutBrand"></h2>
-  <img id="productImage" class="divider" src="" alt="" />
+  <img id="productImage" class="divider" src="${product.Image}" alt="" />
   <p class="product-card__price" id="productFinalPrice"></p>
   <p class="product__color" id="productColorName"></p>
   <p class="product__description" id="productDescriptionHtmlSimple"></p>
   <div class="product-detail__add">
-
   `
+
+
+//   <main class="divider">
+//   <section class="product-detail">
+//     <h3>${product.Brand.Name}</h3>
+
+//     <h2 class="divider">${product.Name}</h2>
+
+//     <img
+//       class="divider"
+//       src="${product.Image}"
+//       alt="${product.Name}"
+//     />
+
+//     <p class="product-card__price">${product.ListPrice</p>
+
+//     <p class="product__color">Rust/Clay</p>
+
+//     <p class="product__description">
+//       Lightweight and ready for adventure, this Cedar Ridge Rimrock tent
+//       boasts a weather-ready design that includes a tub-style floor and
+//       factory-sealed rain fly
+//     </p>
+
+//     <div class="product-detail__add">
+//       <button id="addToCart" data-id="344YJ">Add to Cart</button>
+//     </div>
+//   </section>
+// </main>
+
+
+
 }
