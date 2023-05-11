ebba7f852b01c3dce9e34fcecbf349e69f8686a7
jordankraude <jordankraudeTP@gmail.com>
2023-05-11 15:46:47 -0600

    final push up

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,8 +1,8 @@
 <script>
     import { getData } from "../productData.mjs";
     export let category;
+    export let allowed = ["880RR","985RF","985PR","344YJ"]
     let tentsJson = getData(category);
-
 </script>
     
     <h1>Top products: {category}</h1>
@@ -11,6 +11,7 @@
     {:then products}
     <ul class="product-list">
     {#each products as product}
+    {#if allowed.includes(product.Id)}
         <li class="product-card">
           <a href="product_pages/index.html?product={product.Id}">
             <img
@@ -22,6 +23,7 @@
             <p class="product-card__price">{product.FinalPrice}</p></a
           >
         </li>
+    {/if}
     {/each}
     </ul>
     {/await}
