2514026d7acd5be82145c249df8db46043ca6d2e
jordankraude <jordankraudeTP@gmail.com>
2023-06-13 06:59:28 -0600

    final push ready to merge

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -7,7 +7,11 @@
      products = await getData(category);
   })
 
+ let screenSize;
+
 </script>
+
+<svelte:window bind:innerWidth={screenSize} />
 
 <h1>Top products: {category}</h1>
 
@@ -15,7 +19,15 @@
     {#each products as product}
         <li class="product-card">
           <a href="../product_pages/index.html?product={product.Id}">
+            {#if screenSize > 1024}
+            <img src={product.Images.PrimaryExtraLarge} alt={product.Name} />
+            {:else if screenSize > 640}
+            <img src={product.Images.PrimaryLarge} alt={product.Name} />
+            {:else if screenSize > 320}
             <img src={product.Images.PrimaryMedium} alt={product.Name} />
+            {:else}
+            <img src={product.Images.PrimarySmall} alt={product.Name} />
+            {/if}
             <h3 class="card__brand">{product.Brand.Name}</h3>
             <h2 class="card__name">{product.Name}</h2>
             <div class="product-cardPrices">
