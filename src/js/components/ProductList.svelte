bc0378d57a55522b8fe43296960677de8356a99b
jordankraude <jordankraudeTP@gmail.com>
2023-06-14 09:41:31 -0600

    final push up

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,13 +1,14 @@
 <script>
   import { onMount } from "svelte";
   import { getData } from "../productData.mjs";
+  import { getLocalStorage } from "../utils.mjs";
   export let category;
+
+
  let products = [];
   onMount(async ()=> {
      products = await getData(category);
   })
-
- let screenSize;
 
 function removeQuickView(){
   console.log('running')
@@ -138,30 +139,25 @@ function showCard(event){
 
 </script>
 
-<svelte:window bind:innerWidth={screenSize} />
-
 <h1>Top products: {category}</h1>
 
   <ul class="product-list">
     {#each products as product}
-        <li class="product-card">
+        <li class="product-card" id={product.Id}>
           <a href="../product_pages/index.html?product={product.Id}">
-            {#if screenSize > 1024}
-            <img src={product.Images.PrimaryExtraLarge} alt={product.Name} />
-            {:else if screenSize > 640}
-            <img src={product.Images.PrimaryLarge} alt={product.Name} />
-            {:else if screenSize > 320}
             <img src={product.Images.PrimaryMedium} alt={product.Name} />
-            {:else}
-            <img src={product.Images.PrimarySmall} alt={product.Name} />
-            {/if}
             <h3 class="card__brand">{product.Brand.Name}</h3>
             <h2 class="card__name">{product.Name}</h2>
-            <div class="product-cardPrices">
-              <p class="product-card__price"><s>${product.SuggestedRetailPrice}</s></p>
-              <p class="product-card__price"><b>${product.FinalPrice}</b></p>
-            </div>
+          </a>
+            <div class="productBottom">
+            <a href="../product_pages/index.html?product={product.Id}">
+              <div class="product-cardPrices">
+                <p class="product-card__price"><s>${product.SuggestedRetailPrice}</s></p>
+                <p class="product-card__price"><b>${product.FinalPrice}</b></p>
+              </div>
             </a>
+            <button class="nodisplay" on:click={showCard}><img class="quickView" src="..\public\images\logos\QuickView_30234.png" alt="Quick View Logo"></button>
+          </div>
         </li>
     {/each}
   </ul>
